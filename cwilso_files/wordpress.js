ed6eb85532d6280b73154a041bcb17241967f081
Chris Wilson <cwilso@gmail.com>
2013-09-09 16:16:39 -0700

    initial

---
+++ cwilso_files/wordpress.js
@@ -0,0 +1,12 @@
+//Javascript Document
+function skimlinks() {
+	var skimlinks_pub_id = window.skimlinks_pub_id.toUpperCase();
+
+	var skimlinks_script = document.createElement('script'); 
+		skimlinks_script.type = 'text/javascript'; 
+		skimlinks_script.async = true;
+		skimlinks_script.src = 'http://s.skimresources.com/js/725X1342.skimlinks.js';
+
+	var skimlinks_script_attach = document.getElementsByTagName('script')[0]; 
+		skimlinks_script_attach.parentNode.insertBefore(skimlinks_script, skimlinks_script_attach.nextSibling);
+}
